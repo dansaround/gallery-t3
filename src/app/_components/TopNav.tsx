2e0d5b600068b42ea1d1805c8a01bd3d69a5e142
Daniel Kcomt <kcomtdaniel@gmail.com>
2024-12-02 22:14:38 -0500

    added with jean

--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -4,7 +4,7 @@ import { SimpleUploadButton } from "./UploadButton";
 export default function TopNav() {
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
-      <div>Gallery</div>
+      <div>Gallery with Jean</div>
 
       <div className="pointer flex flex-row items-center gap-4">
         <SignedOut>
