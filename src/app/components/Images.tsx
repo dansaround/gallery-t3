a9ebf692f51ca9c6dbaea87e872cb0ddd66b2c47
Daniel Kcomt <kcomtdaniel@gmail.com>
2024-12-07 11:36:31 -0500

    standarized image height

--- src/app/components/Images.tsx
+++ src/app/components/Images.tsx
@@ -6,18 +6,20 @@ export default async function Images() {
   const images = await getMyImages();
 
   return (
-    <div className="flex flex-wrap justify-center gap-7 p-4">
+    <div className="flex h-fit flex-wrap justify-center gap-7 p-4">
       {images.map((image, index) => (
-        <div key={image.id} className="flex h-48 w-48 flex-col">
-          <Link href={`/img/${image.id}`}>
-            <Image
-              width={192}
-              height={192}
-              src={image.url}
-              alt={`Image ${image.name}`}
-              style={{ objectFit: "contain" }}
-            />
-          </Link>
+        <div key={image.id} className="flex h-52 w-52 flex-col">
+          <div className="relative h-full w-full overflow-hidden">
+            <Link href={`/img/${image.id}`}>
+              <Image
+                width={192}
+                height={192}
+                src={image.url}
+                alt={`Image ${image.name}`}
+                style={{ objectFit: "contain" }}
+              />
+            </Link>
+          </div>
           <div className="text-wrap break-words">{image.name}</div>
         </div>
       ))}
